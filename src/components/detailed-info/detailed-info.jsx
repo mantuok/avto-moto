b36cc6f7884ced36395aeabc91a1580a4968f5a3
mantuok <mantuok@gmail.com>
2021-05-29 02:38:10 +0300

    short and detailed description styling finished

--- src/components/detailed-info/detailed-info.jsx
+++ src/components/detailed-info/detailed-info.jsx
@@ -17,7 +17,7 @@ const DetailedInfo = () => {
 
   return (
     <section className="main__detailed-info detailed-info">
-      <div className="detailedInfo__tabs">
+      <div className="detailed-info__tabs">
         <button className="detailed-info__tab detailed-info__tab--active">Характеристики</button>
         <button className="detailed-info__tab">Отзывы</button>
         <button className="detailed-info__tab">Контакты</button>
