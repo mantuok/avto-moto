5807a5c49d4cf8ff9a1143741d4ad1afa9bea393
mantuok <mantuok@gmail.com>
2021-05-29 02:15:36 +0300

    short and detailed description styling WIP

--- src/components/short-info/short-info.jsx
+++ src/components/short-info/short-info.jsx
@@ -5,10 +5,10 @@ const ShortInfo = () => {
     <section className="main__short-info short-info">
       <h2 className="short-info__heading">Марпех 11</h2>
       <ul className="short-info__short-features short-features">
-        <li className="short-features__item">бензин</li>
-        <li className="short-features__item">механика</li>
-        <li className="short-features__item">100 л.с.</li>
-        <li className="short-features__item">1.4 л</li>
+        <li className="short-features__item short-features__item--petrol">бензин</li>
+        <li className="short-features__item short-features__item--transmission">механика</li>
+        <li className="short-features__item short-features__item--power">100 л.с.</li>
+        <li className="short-features__item short-features__item--size">1.4 л</li>
       </ul>
       <div className="short-info__price price">
         <span className="price price--new">2 300 000 ₽</span>
